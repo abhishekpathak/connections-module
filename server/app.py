2c539b6f3173e867b20b415272910b13da09e8d2
Abhishek Pathak <4abhishekpathak@gmail.com>
2019-09-22 20:07:06 +0530

    fix: move the bootstrap logic to a separate file

---
+++ server/app.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+import os
+from importlib import import_module
+
+from flask import Flask, jsonify
+from flask_cors import CORS
+from flask_restful import Api
+
+from server.exceptions import HttpError
+from server.settings import log
+
+app = Flask(__name__)
+
+# enable CORS for this app
+CORS(app)
+
+api = Api(app, catch_all_404s=True, prefix='/api/v1')
+
+# SOCIAL_APP_MODE can have values either 'prod' or 'dev'
+mode = os.environ.get('SOCIAL_APP_MODE', 'dev').lower()
+
+config = import_module('server.settings.' + mode)
+
+log.configure_logging(config.log_config_file)
+
+from server import views
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
